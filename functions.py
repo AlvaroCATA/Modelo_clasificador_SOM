2c1b74ee74266294547083216398de992db6bbbc
alvaro <callejasta@gmail.com>
2021-03-05 17:25:12 -0600

    Funciones de propósito general utilizada

--- functions.py
+++ functions.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 """
 Created on Thu Mar  4 18:35:55 2021
 
@@ -6,6 +5,7 @@ Created on Thu Mar  4 18:35:55 2021
 """
 import copy
 import numpy as np
+import math as math
 
 ###############################################################################
 """
@@ -78,3 +78,43 @@ def _topographic_error(data,neurons,size_map_y):
         neurons_c[BMU_1,:] = neurons_c[BMU_1,:]-100000
     top_error = error / data.shape[0]
     return top_error
+    
+###############################################################################
+
+###############################################################################
+"""
+Retorna la entropía de una matriz
+"""
+def _get_entropy(matrix):
+    unique_elements,counts_elements = np.unique(matrix, return_counts=True)  
+    entropy = 0    
+    for i in range(len(unique_elements)):
+        pi= counts_elements[i]/(matrix.shape[0]*matrix.shape[1])
+        #entropy += (pi*(math.log(pi,len(unique_elements))))
+        entropy += (pi*(math.log2(pi)))
+    entropy = entropy*-1
+    return entropy
+###############################################################################
+    
+###############################################################################
+    
+def _get_image(file_name,M,shiftm,N,shiftn):
+    matrix = np.zeros((625,460))
+    cluster = np.genfromtxt(file_name,delimiter=',')
+    cnt = 0
+    init_downup = 0
+    end_downup = M
+    init_leftright = 0
+    end_leftright = N
+    lowerBorderImg = int((625-(M-shiftm))/(shiftm))
+    for i in range (cluster.shape[0]):
+        matrix[init_downup:end_downup,init_leftright:end_leftright] = cluster[cnt]
+        init_downup += shiftm
+        end_downup += M
+        if (i%lowerBorderImg) == 0 and i!=0 :
+            init_downup = 0
+            end_downup = M
+            init_leftright += shiftn
+            end_leftright += N
+        cnt += 1
+    return matrix
